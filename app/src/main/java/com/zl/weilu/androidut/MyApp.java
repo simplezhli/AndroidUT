888d8ea049bb65b08d01701c26f8f5c82b21ee38
weilu <a05111993@163.com>
2017-12-03 22:52:10 +0800

    添加Robolectric部分使用示例

---
+++ app/src/main/java/com/zl/weilu/androidut/MyApp.java
@@ -0,0 +1,15 @@
+package com.zl.weilu.androidut;
+
+import android.app.Application;
+
+/**
+ * Created by weilu on 2017/12/3.
+ */
+
+public class MyApp extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/zl/weilu/androidut/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.zl.weilu.androidut.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zl.weilu.androidut.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
